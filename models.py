0a795afccd55694de8e97d0b69a2234ea1a04fb0
jahid hasan <107705267+jami3500@users.noreply.github.com>
2024-11-12 15:25:06 +0600

    notificiation

--- models.py
+++ models.py
@@ -1,50 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-import PIL
-from PIL import Image
-from django.db.models.base import Model
-from django.db.models.fields import DateField
-from django.urls import reverse
-from django.db.models.signals import post_save
-import uuid
-from django.utils import timezone
-from post.models import Post
+# from post.models import Post
 
+class Notification(models.Model):
+    NOTIFICATION_TYPES = ((1, 'Like'), (2, 'Comment'), (3, 'Follow'))
+    post = models.ForeignKey("post.Post", on_delete=models.CASCADE, related_name="notification_post", null=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_from_user" )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_to_user" )
+    notification_types = models.IntegerField(choices=NOTIFICATION_TYPES, null=True, blank=True)
+    text_preview = models.CharField(max_length=100, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_seen = models.BooleanField(default=False)
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_pciture", null=True, default="default.jpg")
-    first_name = models.CharField(max_length=200, null=True, blank=True)
-    last_name = models.CharField(max_length=200, null=True, blank=True)
-    bio = models.CharField(max_length=200, null=True, blank=True)
-    location = models.CharField(max_length=200, null=True, blank=True)
-    url = models.URLField(max_length=200, null=True, blank=True)
-    favourite = models.ManyToManyField(Post, blank=True)
+    # def __str__(self):
+    #     return self.text_preview
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'{self.user.username} - Profile'
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-        if img.height > 250 or img.width > 250:
-            output_size = (250, 250)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
-
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-
-post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
--- urls.py
+++ urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
-from authy.views import UserProfile, EditProfile
+from notification.views import ShowNotification, DeleteNotification
 
 urlpatterns = [
-    # Profile Section
-    path('profile/edit', EditProfile, name="editprofile"),
+    path('', ShowNotification, name='show-notification'),
+    path('<noti_id>/delete', DeleteNotification, name='delete-notification'),
 
-    # User Authentication
-    path('sign-up/', views.register, name="sign-up"),
-    path('sign-in/', auth_views.LoginView.as_view(template_name="sign-in.html", redirect_authenticated_user=True), name='sign-in'),
-    path('sign-out/', auth_views.LogoutView.as_view(template_name="sign-out.html"), name='sign-out'),
 ]
+
+
+
